921d4b602614dbc845d7c4744ca843fd9fff6a68
Jimson <97458912+Jims0n@users.noreply.github.com>
2026-02-16 13:37:55 +0100

    Update contact description with new name

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import ContactClient from "./ContactClient";
 export const metadata: Metadata = {
   title: "Contact",
   description:
-    "Get in touch with Abdulateef Jimson. Have a project in mind or want to work together? Send a message!",
+    "Get in touch with Abdulateef Opeyemi. Have a project in mind or want to work together? Send a message!",
 };
 
 export default function Contact() {
